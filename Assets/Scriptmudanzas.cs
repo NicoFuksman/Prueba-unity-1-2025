5db56a6f363b33a54d8dfcca7e7f1b7feb47dc97
NicoFuksman <bf0005@est.ort.edu.ar>
2025-04-28 11:47:18 -0300

    bueno

---
+++ Assets/Scriptmudanzas.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scriptmudanzas : MonoBehaviour
+{
+    public int peso;
+    public int distancia;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int horas = distancia * 25;
+        int unitarioprecio = horas * 3000;
+        int pickupprecio = horas * 4500;
+        int camionprecio = horas * 7000;
+
+
+
+        if (peso < 100)
+        {
+            Debug.Log("Cantidad de kilos de carga muy baja");
+            return;
+        }
+        if (distancia < 1)
+        {
+            Debug.Log("La distancia no puede ser menor a 1km");
+            return;
+        }
+        if (horas < 2)
+        {
+            horas = 2;
+        }
+        if (distancia > 50)
+        {
+            horas = horas + 1;
+        }
+        if (peso < 2000)
+        {
+            Debug.Log("El vehiculo que necesitas es un unitario");
+            Debug.Log("Tardara" + horas + "horas en llegar");
+            Debug.Log("La mudanza costara" + unitarioprecio);
+            return;
+        }
+        else if (peso > 2000)
+        {
+            Debug.Log("El vehiculo que necesitas es un pickup");
+            Debug.Log("Tardara" + horas + "horas en llegar");
+            Debug.Log("La mudanza costara" + pickupprecio);
+            return;
+        }
+        else if (peso > 5000)
+        {
+            Debug.Log("El vehiculo que necesitas es un camion");
+            Debug.Log("Tardara" + horas + "horas en llegar");
+            Debug.Log("La mudanza costara" + camionprecio);
+            return;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
